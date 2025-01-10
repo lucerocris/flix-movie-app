346ff3e15f2fcf647c1f70d74cb0c73ae471e7a6
Cris Lucero <lucerocris22@gmail.com>
2025-01-10 11:10:42 +0800

    Added popular TV Shows

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular Movies
+
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
 
@@ -20,7 +22,7 @@ async function displayPopularMovies() {
                 : `<img
               src="images/no-image.jpg"
               class="card-img-top"
-              alt="Movie Title"
+              alt="${movie.title}"
             />`
             }
           </a>
@@ -32,6 +34,41 @@ async function displayPopularMovies() {
           </div>
         `;
     document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Display 20 most popular tv shows
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#popular-shows').appendChild(div);
   });
 }
 
@@ -85,7 +122,7 @@ function init() {
       console.log('Search');
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/tv-details.html':
       console.log('TV Details');
