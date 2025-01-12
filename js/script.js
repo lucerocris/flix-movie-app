a06f093c6a0d704947a0067a433d66bf41cb221f
Cris Lucero <lucerocris22@gmail.com>
2025-01-12 11:06:59 +0800

    Added displaySearchResults

--- js/script.js
+++ js/script.js
@@ -253,11 +253,51 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if ((global.search.term !== '') & (global.search.term !== null)) {
-    const results = searchAPIData();
+    const { results, total_pages, page } = await searchAPIData();
     console.log(results);
+
+    if (results.length === 0) {
+      showAlert('No results found');
+      return;
+    }
+
+    displaySearchResults(results);
+    document.querySelector('#search-term').value = '';
   } else {
-    showAlert('Please enter a search term');
+    showAlert('Please enter a search term', 'error');
   }
+}
+
+function displaySearchResults(results) {
+
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+            </p>
+          </div>
+        `;
+    document.querySelector('#search-results').appendChild(div);
+  });
 }
 
 // Display Slider Movies
