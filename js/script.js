5f5220857a13d06f6cd9cbfcb679e36aedb31aa9
Cris Lucero <lucerocris22@gmail.com>
2025-01-12 12:41:25 +0800

    Added pagination

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const global = {
     term: '',
     type: '',
     page: 1,
-    totalPages: 1,
+    totalPage: 1,
     totalResults: 0,
   },
 
@@ -255,7 +255,6 @@ async function search() {
 
   if ((global.search.term !== '') & (global.search.term !== null)) {
     const { results, total_pages, page, total_results } = await searchAPIData();
-    console.log(results);
 
     global.search.page = page;
     global.search.totalPage = total_pages;
@@ -274,6 +273,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // Clear previous search
+
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -310,18 +314,57 @@ function displaySearchResults(results) {
             </p>
           </div>
         `;
-    
+
     document.querySelector('#search-results').appendChild(div);
   });
 
-  document.querySelector('#search-results-heading').innerHTML = 
-  `
+  document.querySelector('#search-results-heading').innerHTML = `
     <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
-  `
- 
+  `;
+  displayPagination();
 }
 
+// Create & display pagination for search
 
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+          <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPage}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable prev if on first page
+
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next if on last page
+
+  if (global.search.page === global.search.totalPage) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev page
+
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+}
 
 // Display Slider Movies
 
@@ -409,7 +452,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
